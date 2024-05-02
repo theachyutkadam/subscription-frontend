d685ff6a89c38c62549f78db0bfd70f22a78662f
theachyutkadam <theachyutkadam@gmail.com>
2024-05-02 16:43:23 +0530

    Update authorzation path

--- src/app/connections/api-config.ts
+++ src/app/connections/api-config.ts
@@ -2,4 +2,4 @@ export const api_base_url = 'http://localhost:8080/api/'
 export const api_url = 'http://localhost:8080'
 
 // export const authToken = sessionStorage.getItem('authToken');
-export const authToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImlhdCI6MTcxNDQ2ODg3Mn0.Lw6NbyZW96zDCUEDw3mLTCqFfPkuiYvslgf3l8Pd1VY'
+export const authToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ik5hdGhhbmFlbDY3QGhvdG1haWwuY29tIiwiaWF0IjoxNzE0NjQ3MzQxfQ.IkHMDX7dca07sOOPrT3kA8RXWeDBDqaSmew8yaj-9gE'
