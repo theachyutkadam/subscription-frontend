fc40ff25e055179a9515a5f08c4d9ac1a4978346
theachyutkadam <theachyutkadam@gmail.com>
2024-05-03 10:38:28 +0530

    Show user details on table

--- src/app/connections/api-config.ts
+++ src/app/connections/api-config.ts
@@ -2,4 +2,4 @@ export const api_base_url = 'http://localhost:8080/api/'
 export const api_url = 'http://localhost:8080'
 
 // export const authToken = sessionStorage.getItem('authToken');
-export const authToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ik5hdGhhbmFlbDY3QGhvdG1haWwuY29tIiwiaWF0IjoxNzE0NjQ3MzQxfQ.IkHMDX7dca07sOOPrT3kA8RXWeDBDqaSmew8yaj-9gE'
+export const authToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkZyaWRhX1NjaHVwcGVAaG90bWFpbC5jb20iLCJpYXQiOjE3MTQ3MTIyMjR9.z73vULSypnnbyQ_m2x5yeF-9jiDyOYCH_5kBR_9jvqs'
